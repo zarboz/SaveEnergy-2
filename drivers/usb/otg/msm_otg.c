087a99bdfb50a6a2eae11a1cf483de950281ce94
BrateloSlava <d_slava@list.ru>
2014-01-09 08:09:45 +0200

    ver-010 - 08.01.2014 + Revert "USB force fast charging". ported from (https://github.com/faux123/htc-m7/commit/2fc85d234bd28f8a2eb76483faabd8d16a2ea0ae) + fastcharge: initial adaptation for HTC One. ported from (https://github.com/faux123/htc-m7/commit/20c36e0338e2ff49fc72ee828c7123202a76fc6f)

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -52,7 +52,6 @@
 
 #ifdef CONFIG_FORCE_FAST_CHARGE
 #include <linux/fastchg.h>
-#define USB_FASTCHG_LOAD 1000 /* uA */
 #endif
 
 #define MSM_USB_BASE	(motg->regs)
@@ -1269,17 +1268,13 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 			"Failed notifying %d charger type to PMIC\n",
 							motg->chg_type);
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge > 0)
+		mA = IDEV_ACA_CHG_MAX;
+#endif
+
 	if (motg->cur_power == mA)
 		return;
-
-#ifdef CONFIG_FORCE_FAST_CHARGE
-  if (force_fast_charge == 1) {
-      mA = USB_FASTCHG_LOAD;
-      pr_info("USB fast charging is ON - 1000mA.\n");
-  } else {
-    pr_info("USB fast charging is OFF.\n");
-  }
-#endif
 
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
 
